2b0ec976dad9f6f724b774136c3d22ce64437a15
Rudy Zidan <rz.zidan@gmail.com>
2022-06-17 18:37:08 +0300

    fix typo

--- api/test/services/update_comment_service_test.rb
+++ api/test/services/update_comment_service_test.rb
@@ -13,7 +13,7 @@ class UpdateCommentServiceTest < ActiveSupport::TestCase
     assert_equal(Comment.name, comment.class.name)
     assert_equal(true, comment.errors.empty?)
     assert_equal("This is a test content", comment.content)
-    assert_broadcasts("post_comment_update#{@comment.post_id}", 1)
+    assert_broadcasts("post_comment_update_#{@comment.post_id}", 1)
   end
 
   test "run with blank params" do
@@ -23,7 +23,7 @@ class UpdateCommentServiceTest < ActiveSupport::TestCase
     assert_equal(true, comment.errors.any?)
 
     assert_equal("can't be blank", comment.errors[:content].first)
-    assert_broadcasts("post_comment_update#{@comment.post_id}", 0)
+    assert_broadcasts("post_comment_update_#{@comment.post_id}", 0)
   end
 
   test "run with different user" do
@@ -35,7 +35,7 @@ class UpdateCommentServiceTest < ActiveSupport::TestCase
     assert_equal(true, comment.errors.any?)
 
     assert_equal("the original post or the user are not allowed to be changed", comment.errors[:comment].first)
-    assert_broadcasts("post_comment_update#{@comment.post_id}", 0)
+    assert_broadcasts("post_comment_update_#{@comment.post_id}", 0)
   end
 
   private
